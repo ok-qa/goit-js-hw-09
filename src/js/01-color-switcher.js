66ad7149f8a62425deb99cffce0c7fb35aa6bc3c
ok-qa <olechka.kr@gmail.com>
2023-07-04 08:47:23 -0700

    Color-switcher ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
+}
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+  
+function onStart() {
+  startBtn.disabled = true;
+  intervalId = setInterval(() => {
+document.body.style.background = getRandomHexColor();
+}, 1000);
+}
+
+function onStop() {
+      startBtn.disabled = false;
+      clearInterval(intervalId);
+}
