90155149486b876d1d0423b47fe00a327e1fea36
ok-qa <olechka.kr@gmail.com>
2023-07-05 00:02:20 -0700

    added comments to clorSwitcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,14 +1,17 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 
+//create random color
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
 }
 
+// add eventListeners to buttons
 startBtn.addEventListener('click', onStart);
 stopBtn.addEventListener('click', onStop);
 
-  
+
+//start changing BG color and disable "Start" button
 function onStart() {
   startBtn.disabled = true;
   intervalId = setInterval(() => {
@@ -16,6 +19,7 @@ document.body.style.background = getRandomHexColor();
 }, 1000);
 }
 
+//stop changing, "Start" button enabled
 function onStop() {
       startBtn.disabled = false;
       clearInterval(intervalId);
