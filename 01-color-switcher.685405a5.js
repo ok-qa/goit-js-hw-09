862852fae344d145fde30b6b660a2701daa82559
ok-qa <69260710+ok-qa@users.noreply.github.com>
2023-07-06 12:45:39 +0000

    Deploying to gh-pages from @ ok-qa/goit-js-hw-09@eb07496371f6480c40da9abdac0b71ac61088819 🚀

---
+++ 01-color-switcher.685405a5.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let o=null;t.addEventListener("click",function(){o=setInterval(()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`,console.log("current color is "+document.body.style.background)},1e3),t.disabled=!0}),e.addEventListener("click",function(){clearInterval(o),t.disabled=!1});
+//# sourceMappingURL=01-color-switcher.685405a5.js.map
--- 01-color-switcher.c65db10a.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n=null;t.addEventListener("click",function(){n=setInterval(()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`},1e3),t.disabled=!0}),e.addEventListener("click",function(){clearInterval(n),t.disabled=!1});
-//# sourceMappingURL=01-color-switcher.c65db10a.js.map
