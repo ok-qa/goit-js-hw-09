047c0b2062746e4f4fc0de39ce17711c462d026a
ok-qa <69260710+ok-qa@users.noreply.github.com>
2023-07-06 09:41:59 +0000

    Deploying to gh-pages from @ ok-qa/goit-js-hw-09@a9cda6a7468988403fdebc5ac6a09ef45921654a 🚀

---
+++ 01-color-switcher.f88c24a6.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");e.addEventListener("click",function(){t&&(t=setInterval(function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.background=t},1e3),e.disabled=!0)}),n.addEventListener("click",function(){clearInterval(t),t=void 0,e.disabled=!1})}();
+//# sourceMappingURL=01-color-switcher.f88c24a6.js.map
