f408ed0393add027ce430c18021fe571e17d0cf1
ok-qa <olechka.kr@gmail.com>
2023-07-14 06:35:50 -0700

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -11,190 +11,84 @@ const dataMinutes = document.querySelector('[data-minutes]');
 const dataSeconds = document.querySelector('[data-seconds]');
 
 //initialize picker
-picker('#datetime-picker', {
+const picker = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(chosenDate) {
-    const targetDate = chosenDate[0];
-
-    if (targetDate) {
-      startBtn.disabled = false;
+  onClose(selectedDates) {
+    targetDate = selectedDates[0];
+    if (targetDate.getTime() < options.defaultDate.getTime()) {
+      Notiflix.Report.warning(
+        "Sorry, it is not a valid date",
+        'Please chose a date in the future',
+        'OK'
+      );
+      startBtn.classList.remove('valid-date');
+      startBtn.classList.add('invalid-date');
     } else {
-      startBtn.disabled = true;
+      startBtn.classList.remove('invalid-date');
+      startBtn.classList.add('valid-date');
     }
   },
-});
+};
 
-startBtn.addEventListener('click', () => {
-  const targetDate = picker.value;
-  const timeDifference = calculateTimeDifference(targetDate);
+//chose date and time 
+flatpickr('input#datetime-picker', picker);
 
-  if (timeDifference) {
-    startCountdown(timeDifference);
-    startButton.disabled = true;
-  }
-});
-
-
+let targetDate = null;
+let timer = null;
 const interval = 1000;
 
+startBtn.addEventListener('click', () => {
+  if (targetDate && !timer) {
+    timer = setInterval(() => {
+      let currentDateInMs = new Date().getTime();
+      let timeDiff = targetDate.getTime() - currentDateInMs;
+      if (timeDiff <= 0) {
+        clearInterval(timer);
+        timer = null;
 
-
-
-
-function getTimeSegmentElements(segmentElement) {
-  const segmentDisplay = segmentElement.querySelector('.segment-display');
-  const segmentDisplayTop = segmentDisplay.querySelector(
-    '.segment-display__top'
-  );
-  const segmentDisplayBottom = segmentDisplay.querySelector(
-    '.segment-display__bottom'
-  );
-
-  const segmentOverlay = segmentDisplay.querySelector('.segment-overlay');
-  const segmentOverlayTop = segmentOverlay.querySelector(
-    '.segment-overlay__top'
-  );
-  const segmentOverlayBottom = segmentOverlay.querySelector(
-    '.segment-overlay__bottom'
-  );
-
-  return {
-    segmentDisplayTop,
-    segmentDisplayBottom,
-    segmentOverlay,
-    segmentOverlayTop,
-    segmentOverlayBottom,
-  };
-}
-
-function updateSegmentValues(displayElement, overlayElement, value) {
-  displayElement.textContent = value;
-  overlayElement.textContent = value;
-}
-
-function updateTimeSegment(segmentElement, timeValue) {
-  const segmentElements = getTimeSegmentElements(segmentElement);
-
-  if (
-    parseInt(segmentElements.segmentDisplayTop.textContent, 10) === timeValue
-  ) {
-    return;
-  }
-
-  segmentElements.segmentOverlay.classList.add('flip');
-
-  updateSegmentValues(
-    segmentElements.segmentDisplayTop,
-    segmentElements.segmentOverlayBottom,
-    timeValue
-  );
-
-  function finishAnimation() {
-    segmentElements.segmentOverlay.classList.remove('flip');
-    updateSegmentValues(
-      segmentElements.segmentDisplayBottom,
-      segmentElements.segmentOverlayTop,
-      timeValue
-    );
-
-    this.removeEventListener('animationend', finishAnimation);
-  }
-
-  segmentElements.segmentOverlay.addEventListener(
-    'animationend',
-    finishAnimation
-  );
-}
-
-function startCountdown(time) {
-  let { days, hours, minutes, seconds } = time;
-
-    const countdownInterval = setInterval(() => {
-        if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
-            clearInterval(countdownInterval);
-            Notiflix.Notify.success('Countdown finished!');
-            return;
+        let reloadIcon = document.querySelector('.reload-icon');
+        if (reloadIcon) {
+          document.body.removeChild(reloadIcon);
         }
 
-        seconds--;
+        return;
+      }
 
-        if (seconds < 0) {
-            seconds = 59;
-            minutes--;
-
-            if (minutes < 0) {
-                minutes = 59;
-                hours--;
-
-                if (hours < 0) {
-                    hours = 23;
-                    days--;
-                }
-            }
-        }
-
-        //daysValue.textContent = addLeadingZero(days);
-        //hoursValue.textContent = addLeadingZero(hours);
-        //minutesValue.textContent = addLeadingZero(minutes);
-        //secondsValue.textContent = addLeadingZero(seconds);
-    }); //, 1000);
-
-  function updateTimeSection(sectionID, timeValue) {
-    const firstNumber = Math.floor(timeValue / 10) || 0;
-    const secondNumber = timeValue % 10 || 0;
-    const sectionElement = document.getElementById(sectionID);
-    const timeSegments = sectionElement.querySelectorAll('.time-segment');
-
-    updateTimeSegment(timeSegments[0], firstNumber);
-    updateTimeSegment(timeSegments[1], secondNumber);
-  }
-
-  function getTimeRemaining(targetDateTime) {
-    const nowTime = Date.now();
-    const complete = nowTime >= targetDateTime;
-
-    if (complete) {
-      return {
-        complete,
-        seconds: 0,
-        minutes: 0,
-        hours: 0,
+      let remainingTime = convertMs(timeDiff);
+      const timeUnits = {
+        days: daysSpan,
+        hours: hoursSpan,
+        minutes: minutesSpan,
+        seconds: secondsSpan,
       };
-    }
 
-    const secondsRemaining = Math.floor((targetDateTime - nowTime) / 1000);
-    const hours = Math.floor(secondsRemaining / 60 / 60);
-    const minutes = Math.floor(secondsRemaining / 60) - hours * 60;
-    const seconds = secondsRemaining % 60;
+      // Оновлення відображення часу на сторінці
+      Object.keys(timeUnits).forEach(unit => {
+        timeUnits[unit].textContent = String(remainingTime[unit]).padStart(
+          2,
+          '0'
+        );
+      });
 
-    return {
-      complete,
-      seconds,
-      minutes,
-      hours,
-    };
+      startBtn.style.display = 'none';
+    }, interval);
   }
+});
 
-  function updateAllSegments() {
-    //const timeRemainingBits = getTimeRemaining(new Date(targetDate).getTime());
+// Функція для перетворення мілісекунд в дні, години, хвилини, та секунди
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    updateTimeSection('seconds', timeRemainingBits.seconds);
-    updateTimeSection('minutes', timeRemainingBits.minutes);
-    updateTimeSection('hours', timeRemainingBits.hours);
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-    return timeRemainingBits.complete;
-  }
-
-  const countdownTimer = setInterval(() => {
-    const isComplete = updateAllSegments();
-
-    if (isComplete) {
-      clearInterval(countdownTimer);
-    }
-  }, interval);
-
-  updateAllSegments();
+  return { days, hours, minutes, seconds };
 }
